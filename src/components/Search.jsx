bd7135af1e483cbb3141303df5b7b6f134a2d4f4
Alex <alex.zhelyazko@yandex.by>
2022-09-27 06:01:26 +0400

    add: style

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="find a user" />
+      </div>
+      <div className="userChat">
+        <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
 };
