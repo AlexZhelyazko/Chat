48ecdc0c8666a1f15b1b21557f3f84d92611bd72
Alex <alex.zhelyazko@yandex.by>
2022-09-27 06:11:47 +0400

    add: new styles

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import Cam from '../img/cam.png';
+import Add from '../img/add.png';
+import More from '../img/more.png';
 
 export const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Alex</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
