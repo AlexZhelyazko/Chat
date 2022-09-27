9739f623e9ca22969393e7522925239c17e02cf8
Alex <alex.zhelyazko@yandex.by>
2022-09-27 05:50:43 +0400

    add: navbat

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Chat</span>
+      <div className="user">
+        <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="" />
+        <span>Alex</span>
+        <button>logout</button>
+      </div>
+    </div>
+  );
 };
