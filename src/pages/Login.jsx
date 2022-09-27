bc75448808b7b0a8490cbb83752ba22e9e9764f8
Alex <alex.zhelyazko@yandex.by>
2022-09-27 05:39:56 +0400

    start working with home page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Add from '../img/addAvatar.png';
+
+export const Login = () => {
+  return (
+    <div className="form__container">
+      <div className="form__wrapper">
+        <span className="logo">Chat</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign In</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+};
