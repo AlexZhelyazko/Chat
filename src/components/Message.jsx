4edb3908e9fe2c578bdf73bd20114510a9c02086
Alex <alex.zhelyazko@yandex.by>
2022-09-27 06:58:57 +0400

    end style

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="" />
+      </div>
+    </div>
+  );
 };
