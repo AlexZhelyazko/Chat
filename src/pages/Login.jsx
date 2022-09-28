c3ca739f5047f29d3ed2af8eaa1e4e55bf92c7a5
Alex <alex.zhelyazko@yandex.by>
2022-09-28 06:18:27 +0400

    add: login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import Add from '../img/addAvatar.png';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth, storage, db } from '../firebase';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { doc, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export const Login = () => {
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/login');
+    } catch (err) {
+      setError(true);
+    }
+  };
   return (
     <div className="form__container">
       <div className="form__wrapper">
         <span className="logo">Chat</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
           <button>Sign In</button>
+          {error && <span>Something went wrong</span>}
         </form>
         <p>You don't have an account? Register</p>
       </div>
