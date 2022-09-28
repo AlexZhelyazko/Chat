3092cee05f6d7bc9eae86028689aaad5ad96580b
Alex <alex.zhelyazko@yandex.by>
2022-09-28 05:59:26 +0400

    add: logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
 
 export const Navbar = () => {
   return (
@@ -7,7 +9,7 @@ export const Navbar = () => {
       <div className="user">
         <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="" />
         <span>Alex</span>
-        <button>logout</button>
+        <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
   );
